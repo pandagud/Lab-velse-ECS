436e596ad29da191b160b7fcd823cf3c758280ce
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-02-15 11:42:18 +0100

    Created Test

---
+++ UnitTestProjectForECS/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestProjectForECS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
